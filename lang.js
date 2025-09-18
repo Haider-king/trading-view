e587de4216d15c5cdf71f9dc48e99b0e5d26c897
Haider-King <haederk544@gmail.com>
2025-09-19 01:35:06 +0300

    a

---
+++ lang.js
@@ -0,0 +1,25 @@
+
+
+const languageSelector = document.querySelector('select');
+languageSelector.addEventListener("change", (event) => {
+    setLanguage(event.target.value);
+    localStorage.setItem('lang', event.target.value);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    setLanguage(localStorage.getItem('lang') || 'en');
+});
+
+const setLanguage = (language) => {
+    const elements = document.querySelectorAll('[data-i18n]');
+    elements.forEach((element) => {
+        const translationKey = element.getAttribute('data-i18n');
+        element.textContent = translations[language][translationKey];
+    });
+    if (language === 'ar') {
+        document.body.dir = 'rtl';
+    } else {
+        document.body.dir = 'ltr';
+    }
+}
+
---
+++ translations.js
@@ -0,0 +1,50 @@
+const translations = {
+    en: {
+        home: "Home",
+        product: "Product",
+        pricing: "Pricing",
+        about: "About",
+        contact: "Contact",
+        login: "Login",
+        signup: "Sign Up",
+        hero_title: "Powerful. Simplicity...",
+        hero_description: "Just type what's on your mind and we'll get........",
+        hero_cta: "Try For Free"
+    },
+    ar: {
+        home: "الصفحة الرئيسية",
+        product: "المنتج",
+        pricing: "التسعير",
+        about: "معلومات عنا",
+        contact: "اتصل",
+        login: "تسجيل الدخول",
+        signup: "التسجيل",
+        hero_title: "قوة وبساطة...",
+        hero_description: "اكتب فقط ما يخطر ببالك وسنقوم بالباقي...",
+        hero_cta: "جرّب مجانًا"
+    },
+    fr: {
+        home: "Accueil",
+        about: "À propos",
+        product: "Produit",
+        pricing: "Tarification",
+        contact: "Contact",
+        login: "Connexion",
+        signup: "S'inscrire",
+        hero_title: "Puissant. Simplicité...",
+        hero_description: "Tapez simplement ce que vous pensez et nous nous occupons du reste...",
+        hero_cta: "Essayer gratuitement"
+    },
+    ru: {
+        home: "Главная",
+        about: "О нас",
+        product: "Продукт",
+        pricing: "Цены",
+        contact: "Контакт",
+        login: "Вход",
+        signup: "Регистрация",
+        hero_title: "Мощь и простота...",
+        hero_description: "Просто напишите то, что у вас на уме, и мы сделаем остальное...",
+        hero_cta: "Попробовать бесплатно"
+    },
+};
